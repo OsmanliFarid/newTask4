f79852ab6caffb1f12f94f49e3e7d0bb718bf057
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-28 17:05:31 +0400

    task6

---
+++ task6/app.js
@@ -0,0 +1,24 @@
+/*
+--- STACK ---
+
+
+1-Melumatlari daxil olmaq cox suretlidi
+2-limitli yaddasi var yeni olcusu proqramin baslangicinda teyin edilir
+3-funksiyalari cagirildiqda  ve geri dondukde istifade olunur
+
+
+--- HEAP ---
+
+1-Stackdan daha yavasdir
+2-dinamik yaddasa sahibdir proqramin islediyi vaxtda istediyiniz qeder yaddas ayira bilersiz
+3- Melumatlari istediyin kimi yerlesdire bilersen
+
+--- NETICE ---
+
+HEAP dinamik yaddas ucun,STACK ise muveqqeti yaddas ucun istifade olunur her ikisi proqramlarin 
+isinde ehemiyyetli rol oynayir
+
+
+
+
+*/
