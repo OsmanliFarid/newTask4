0bb0160bd99ec7ca42358d92baf2bfc3ef4b2829
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-28 17:05:25 +0400

    task5

---
+++ task5/app.js
@@ -0,0 +1,13 @@
+/*
+5. propertyTaker() adinda funksiya yaradin. Argument kimi obyekt (obj) ve string (proprtyName) 
+alsin. Hemin obyektin verdiyim propertyni ekrana cixarsin. Mes: */
+
+
+function propertyTaker(obj,proprtyName) {
+    console.log(obj[proprtyName])
+    
+    
+}
+propertyTaker({ continent: 'Asia', country: 'Japan'}, 'continent')
+
+
